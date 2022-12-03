e50853b86aca017d68123b80efb434f69b6acfd9
szymon4710 <szymon4710@gmail.com>
2022-12-03 19:25:17 +0100

    Started creating Routing

---
+++ src/controllers/DefaultController.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once 'AppController.php';
+
+class DefaultController extends AppController {
+
+    public function index(){
+        // TODO display login.html
+        die("index method");
+
+    }
+
+    public function events() {
+        // TODO display events.html
+        die("events method");
+    }
+
+
+}
