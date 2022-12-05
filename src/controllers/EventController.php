50d11274553cc74ba3785dca9fd215d8a3d9b464
szymon4710 <szymon4710@gmail.com>
2022-12-05 23:36:18 +0100

    Create Event.php, update EventController

--- src/controllers/EventController.php
+++ src/controllers/EventController.php
@@ -1,11 +1,45 @@
 <?php
 
 require_once 'AppController.php';
+require_once __DIR__.'/../models/Event.php';
+
 
 class EventController extends AppController
 {
-    public function addEvent() {
-        $this->render('addEvent');
+    const MAX_FILE_SIZE = 1024 * 1024;
+    const SUPPORTED_TYPES = ['image/png', 'image/jpeg'];
+    const UPLOAD_DIRECTORY = '/../public/uploads/';
 
+    private $messages = [];
+
+    public function addEvent() {
+        if ($this->isPost() && is_uploaded_file($_FILES['file']['tmp_name']) && $this->validate($_FILES['file'])) {
+
+            move_uploaded_file(
+                $_FILES['file']['tmp_name'],
+                dirname(__DIR__).self::UPLOAD_DIRECTORY.$_FILES['file']['name']
+            );
+
+            $event = new Event($_POST['title'], $_POST['description'], $_FILES['file']['name']);
+
+            return $this->render('events', ['messages' => $this->messages, 'event' => $event]);
+        }
+
+        $this->render('addEvent', ['messages' => $this->messages]);
+    }
+
+    private function validate(array $file) : bool
+    {
+        if ($file['size'] > self::MAX_FILE_SIZE) {
+            $this->messages[] = 'File is too large for destination file system.';
+            return false;
+        }
+
+        if (!isset($file['type']) && !in_array($file['type'], self::SUPPORTED_TYPES)) {
+            $this->messages[] = 'File type is not supported.';
+            return false;
+        }
+
+        return true;
     }
 }
---
+++ src/models/Event.php
@@ -0,0 +1,49 @@
+<?php
+
+class Event
+{
+    private $title;
+    private $description;
+    private $image;
+
+    public function __construct($title, $description, $image)
+    {
+        $this->title = $title;
+        $this->description = $description;
+        $this->image = $image;
+    }
+
+
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    public function setTitle($title): void
+    {
+        $this->title = $title;
+    }
+
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    public function setDescription($description): void
+    {
+        $this->description = $description;
+    }
+
+    public function getImage()
+    {
+        return $this->image;
+    }
+
+    public function setImage($image): void
+    {
+        $this->image = $image;
+    }
+
+
+
+}
