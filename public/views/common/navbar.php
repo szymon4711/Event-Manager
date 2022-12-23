15579ccb3f8d29f825acb4005627c2e73425f521
szymon4710 <szymon4710@gmail.com>
2022-12-24 00:42:43 +0100

    Added navbar.php

---
+++ public/views/common/navbar.php
@@ -0,0 +1,28 @@
+<!-- TODO create common directory and move this file to it-->
+<nav>
+    <img src="public/img/logo.svg">
+    <ul>
+        <li>
+            <i class="fa-regular fa-calendar"></i>
+            <a href="#" class="button">Events</a>
+        </li>
+        <li>
+            <i class="fa-regular fa-heart"></i>
+            <a href="#" class="button">My events</a>
+        </li>
+        <li>
+            <i class="fa-regular fa-bell"></i>
+            <a href="#" class="button">Notices</a>
+        </li>
+        <li>
+            <i class="fa-solid fa-gear"></i>
+            <a href="#" class="button">Settings</a>
+        </li>
+    </ul>
+
+    <button class="burger">
+        <div class="line"></div>
+        <div class="line"></div>
+        <div class="line"></div>
+    </button>
+</nav>
